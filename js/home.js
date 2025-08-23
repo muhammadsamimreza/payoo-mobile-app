78c72e0bb71fcc54e92fe6bb0ab346c6f5a8a6c3
muhammadsamimreza <muhammadsamimreza@gmail.com>
2025-08-24 01:30:39 +0600

    re-useable function create and Apply

--- js/home.js
+++ js/home.js
@@ -1,27 +1,45 @@
+//Function of---->>> Get value of Number from user input data ///
+function getValueInNumber(id) {
+  const valueInNumber = parseInt(document.getElementById(id).value);
+  return valueInNumber;
+}
+
+//Function of---->>> Get just value from user input data ///
+function getJustValue(id) {
+  const justValue = document.getElementById(id).value;
+  return justValue;
+}
+//Function of---->>> Get just value from user input data ///
+function getInnerTextInNumber(id) {
+  const InnerTextInNumber = parseInt(document.getElementById(id).innerText);
+  return InnerTextInNumber;
+}
+
+// Function of ----->>>> Toggle Button ////
+function toggleButton(id) {
+  const commonClassNameItems =
+    document.getElementsByClassName("interface-form");
+  for (const item of commonClassNameItems) {
+    item.style.display = "none";
+  }
+  document.getElementById(id).style.display = "block";
+}
+
 document
   .getElementById("add-money-submit-btn")
   .addEventListener("click", function (e) {
     e.preventDefault();
 
-    const bankName = document.getElementById("bankName").value;
-    const accountNumberBtn = parseInt(
-      document.getElementById("account-number-btn").value
-    );
-    const addAmountInputBtn = parseInt(
-      document.getElementById("add-money-input-field").value
-    );
-    const addPinBtn = parseInt(document.getElementById("pin-number-btn").value);
+    const bankName = getJustValue("bankName");
+    const accountNumberBtn = getValueInNumber("account-number-btn");
+    const addAmountInputBtn = getValueInNumber("add-money-input-field");
+    const addPinBtn = getValueInNumber("pin-number-btn");
 
     // Add Money submit Btn
 
-    const availableBalance = parseInt(
-      document.getElementById("availableBalance").innerText
-    );
-
+    const availableBalance = getInnerTextInNumber("availableBalance");
     const totalCurrentBalance = availableBalance + addAmountInputBtn;
-
     document.getElementById("availableBalance").innerText = totalCurrentBalance;
-
     document.getElementById("add-money-input-field").value = "";
   });
 
@@ -30,112 +48,88 @@ document
 document
   .getElementById("add-money-feature-button")
   .addEventListener("click", function () {
-    document.getElementById("cash-out-feature-section").style.display = "none";
-    document.getElementById('transfer-money-feature-section').style.display = "none";
-    document.getElementById('get-bonus-feature-section').style.display = "none";
-    document.getElementById('pay-bill-feature-section').style.display = "none";
-    document.getElementById('transaction-history-feature-section').style.display = "none";
-    document.getElementById("add-money-feature-section").style.display = "block";
+    toggleButton("add-money-feature-section");
   });
-
 
 //   Cash Out Toggle //
 document
   .getElementById("cash-out-feature-button")
   .addEventListener("click", function () {
-    document.getElementById("add-money-feature-section").style.display = "none";
-    document.getElementById('transfer-money-feature-section').style.display = "none";
-    document.getElementById('get-bonus-feature-section').style.display = "none";
-    document.getElementById('pay-bill-feature-section').style.display = "none";
-    document.getElementById('transaction-history-feature-section').style.display = "none";
-    document.getElementById("cash-out-feature-section").style.display = "block";
+    toggleButton("cash-out-feature-section");
   });
 
 // Cash Out Section //
 
-document.getElementById('cash-out-submit-btn').addEventListener('click', function(e){
+document
+  .getElementById("cash-out-submit-btn")
+  .addEventListener("click", function (e) {
     e.preventDefault();
-    
-    const inputBalanceCatch = parseInt(document.getElementById('cash-out-input-field').value);
 
-    const crrentBalance = parseInt(document.getElementById("availableBalance").innerText);
+    const inputBalanceCatch = parseInt(
+      document.getElementById("cash-out-input-field").value
+    );
 
+    const crrentBalance = parseInt(
+      document.getElementById("availableBalance").innerText
+    );
 
     const afterCashOutBalance = crrentBalance - inputBalanceCatch;
-    
 
-   document.getElementById("availableBalance").innerText = afterCashOutBalance;
+    document.getElementById("availableBalance").innerText = afterCashOutBalance;
 
-   document.getElementById('cash-out-input-field').value ='';
-
-
-})
-
+    document.getElementById("cash-out-input-field").value = "";
+  });
 
 // Transfer Section and Send Now Button ///
 
-document.getElementById('transfer-submit-button').addEventListener('click', function(e){
+document
+  .getElementById("transfer-submit-button")
+  .addEventListener("click", function (e) {
     e.preventDefault();
 
-    const currentBalanceStill = parseInt(document.getElementById("availableBalance").innerText);
+    const currentBalanceStill = parseInt(
+      document.getElementById("availableBalance").innerText
+    );
 
-    const transferAmountInputField = parseInt(document.getElementById('transfer-amount-input-field').value);
+    const transferAmountInputField = parseInt(
+      document.getElementById("transfer-amount-input-field").value
+    );
 
     const afterTransferBalance = currentBalanceStill - transferAmountInputField;
 
-    document.getElementById("availableBalance").innerText = afterTransferBalance;
+    document.getElementById("availableBalance").innerText =
+      afterTransferBalance;
 
-    document.getElementById('transfer-amount-input-field').value = '';
-
-
-
-})
+    document.getElementById("transfer-amount-input-field").value = "";
+  });
 
 // Toggle and Show Transfer feature ///
 
-document.getElementById('trasfer-money-freature-page').addEventListener('click', function(){
-    
-    document.getElementById("cash-out-feature-section").style.display = "none";
-    document.getElementById("add-money-feature-section").style.display = "none";
-    document.getElementById('get-bonus-feature-section').style.display = "none";
-    document.getElementById('pay-bill-feature-section').style.display = "none";
-    document.getElementById('transaction-history-feature-section').style.display = "none";
-    document.getElementById('transfer-money-feature-section').style.display = "block";
-})
-
+document
+  .getElementById("trasfer-money-freature-page")
+  .addEventListener("click", function () {
+    toggleButton("transfer-money-feature-section");
+  });
 
 // Get Bonus site Toggle ///
 
-document.getElementById('get-bonus-feature-site').addEventListener('click', function(){
-    
-    document.getElementById("cash-out-feature-section").style.display = "none";
-    document.getElementById("add-money-feature-section").style.display = "none";
-    document.getElementById('transfer-money-feature-section').style.display = "none";
-    document.getElementById('pay-bill-feature-section').style.display = "none";
-    document.getElementById('transaction-history-feature-section').style.display = "none";
-    document.getElementById('get-bonus-feature-section').style.display = "block";
-})
+document
+  .getElementById("get-bonus-feature-site")
+  .addEventListener("click", function () {
+   toggleButton("get-bonus-feature-section");
+  });
 
 // Pay Bill site Toggle ///
 
-document.getElementById('pay-bill-feature-site').addEventListener('click', function(){
-    
-    document.getElementById("cash-out-feature-section").style.display = "none";
-    document.getElementById("add-money-feature-section").style.display = "none";
-    document.getElementById('transfer-money-feature-section').style.display = "none";
-    document.getElementById('get-bonus-feature-section').style.display = "none";
-    document.getElementById('transaction-history-feature-section').style.display = "none";
-    document.getElementById('pay-bill-feature-section').style.display = "block";
-})
-// Transaction Histry site Toggle ///
+document
+  .getElementById("pay-bill-feature-site")
+  .addEventListener("click", function () {
+  toggleButton("pay-bill-feature-section");
+  });
+// Transaction History site Toggle ///
 
-document.getElementById('transaction-history-feature-site').addEventListener('click', function(){
-    
-    document.getElementById("cash-out-feature-section").style.display = "none";
-    document.getElementById("add-money-feature-section").style.display = "none";
-    document.getElementById('transfer-money-feature-section').style.display = "none";
-    document.getElementById('get-bonus-feature-section').style.display = "none";
-    document.getElementById('pay-bill-feature-section').style.display = "none";
-    document.getElementById('transaction-history-feature-section').style.display = "block";
-    
-})
+document
+  .getElementById("transaction-history-feature-site")
+  .addEventListener("click", function () {
+    toggleButton("transaction-history-feature-section");
+  });
