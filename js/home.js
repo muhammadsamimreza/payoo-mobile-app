8fc77e3239bef5ad0864fdaae5f931cedbad5edc
muhammadsamimreza <muhammadsamimreza@gmail.com>
2025-08-24 15:16:36 +0600

    Changed js with reUseable code

--- js/home.js
+++ js/home.js
@@ -1,3 +1,5 @@
+const dataContainer = [];
+
 //Function of---->>> Get value of Number from user input data ///
 function getValueInNumber(id) {
   const valueInNumber = parseInt(document.getElementById(id).value);
@@ -14,6 +16,17 @@ function getInnerTextInNumber(id) {
   const InnerTextInNumber = parseInt(document.getElementById(id).innerText);
   return InnerTextInNumber;
 }
+//Function of---->>> Get just InnerText from Tag's data ///
+function getInnerText(id) {
+  const InnerText = document.getElementById(id).innerText;
+  return InnerText;
+}
+//Function of---->>> Set InnerText to main Storage ///
+function setInnerText(value) {
+  const mainBalance = document.getElementById("availableBalance");
+  mainBalance.innerText = value;
+  return mainBalance;
+}
 
 // Function of ----->>>> Toggle Button ////
 function toggleButton(id) {
@@ -25,6 +38,34 @@ function toggleButton(id) {
   document.getElementById(id).style.display = "block";
 }
 
+// Function of ----->>>> Toggle Button for (bg, border) Changed ////
+function toggleForCss(id) {
+  const allBtn = document.getElementsByClassName("btnCss");
+
+  for (const singleBtn of allBtn) {
+    singleBtn.classList.remove("border-[#0874f2]", "bg-[#0874f20d]");
+    singleBtn.classList.add("border-[#0808081a]");
+  }
+  document.getElementById(id).classList.remove("border-[#0808081a]");
+  document
+    .getElementById(id)
+    .classList.add("border-[#0874f2]", "bg-[#0874f20d]");
+}
+// Function of acces transaction details ///
+
+function getTransactionDetails(titleId){
+  const data = {
+      name: getInnerText(titleId),
+      date: new Date().toLocaleTimeString(),
+    };
+   return dataContainer.push(data);
+}
+// LogOut Button//
+document.getElementById("logout-btn").addEventListener('click', function(){
+  alert("Are you Sure ??");
+  document.location.href = "./index.html"
+});
+// Add Money submit Btn
 document
   .getElementById("add-money-submit-btn")
   .addEventListener("click", function (e) {
@@ -35,27 +76,12 @@ document
     const addAmountInputBtn = getValueInNumber("add-money-input-field");
     const addPinBtn = getValueInNumber("pin-number-btn");
 
-    // Add Money submit Btn
-
     const availableBalance = getInnerTextInNumber("availableBalance");
     const totalCurrentBalance = availableBalance + addAmountInputBtn;
-    document.getElementById("availableBalance").innerText = totalCurrentBalance;
-    document.getElementById("add-money-input-field").value = "";
-  });
 
-// Add Money feature Button with Toggle ////
-
-document
-  .getElementById("add-money-feature-button")
-  .addEventListener("click", function () {
-    toggleButton("add-money-feature-section");
-  });
-
-//   Cash Out Toggle //
-document
-  .getElementById("cash-out-feature-button")
-  .addEventListener("click", function () {
-    toggleButton("cash-out-feature-section");
+    setInnerText(totalCurrentBalance);
+   document.getElementById("add-money-input-field").value = "";
+    getTransactionDetails("add-money-title");
   });
 
 // Cash Out Section //
@@ -64,20 +90,13 @@ document
   .getElementById("cash-out-submit-btn")
   .addEventListener("click", function (e) {
     e.preventDefault();
-
-    const inputBalanceCatch = parseInt(
-      document.getElementById("cash-out-input-field").value
-    );
-
-    const crrentBalance = parseInt(
-      document.getElementById("availableBalance").innerText
-    );
-
+    const inputBalanceCatch = getValueInNumber("cash-out-input-field");
+    const crrentBalance = getInnerTextInNumber("availableBalance");
     const afterCashOutBalance = crrentBalance - inputBalanceCatch;
-
-    document.getElementById("availableBalance").innerText = afterCashOutBalance;
-
+   
+    setInnerText(afterCashOutBalance);
     document.getElementById("cash-out-input-field").value = "";
+    getTransactionDetails("cash-out-title");
   });
 
 // Transfer Section and Send Now Button ///
@@ -87,20 +106,65 @@ document
   .addEventListener("click", function (e) {
     e.preventDefault();
 
-    const currentBalanceStill = parseInt(
-      document.getElementById("availableBalance").innerText
-    );
-
-    const transferAmountInputField = parseInt(
-      document.getElementById("transfer-amount-input-field").value
-    );
+    const currentBalanceStill = getInnerTextInNumber("availableBalance");
+    const transferAmountInputField = getValueInNumber("transfer-amount-input-field");
 
     const afterTransferBalance = currentBalanceStill - transferAmountInputField;
-
-    document.getElementById("availableBalance").innerText =
-      afterTransferBalance;
-
+    setInnerText(afterTransferBalance);
     document.getElementById("transfer-amount-input-field").value = "";
+    getTransactionDetails("transfer-money-title")
+  });
+
+// Show  >>>** Transaction History **<<< \\
+
+document
+  .getElementById("transaction-history-feature-site")
+  .addEventListener("click", function () {
+    const interfaceDataContainer = document.getElementById(
+      "interface-Data-Container"
+    );
+    interfaceDataContainer.innerText = "";
+    for (const data of dataContainer) {
+      const div = document.createElement("div");
+      div.innerHTML = `
+          <div class="w-[90%] mx-auto bg-white rounded-2xl py-5 border-2 border-gray-200 mb-3">
+            <div class="">
+              <div class="Transaction-Card flex justify-between items-center px-5">
+               <div class="Left-site-transaction flex gap-5">
+                <div class="w-14 rounded-full bg-[#f4f5f7] p-3">
+                 <img class="w-14" src="./assets/wallet1.png" alt="" />
+               </div>
+                <div>
+                  <h2 class="text-xl font-semibold text-[#080808b3]">${data.name}</h2>
+                  <p class="text-[#080808b3]">${data.date}</p>
+                </div>
+               </div>
+                <div class="dot text-2xl text-[#080808b3]">
+                  <i class="cursor-pointer fa-solid fa-ellipsis-vertical"></i>
+                </div>
+              </div>
+            </div>
+          </div>
+    `;
+      interfaceDataContainer.appendChild(div);
+    }
+  });
+
+// Add Money Toggle ////
+
+document
+  .getElementById("add-money-feature-button")
+  .addEventListener("click", function () {
+    toggleButton("add-money-feature-section");
+    toggleForCss("add-money-feature-button");
+  });
+
+//   Cash Out Toggle //
+document
+  .getElementById("cash-out-feature-button")
+  .addEventListener("click", function () {
+    toggleButton("cash-out-feature-section");
+    toggleForCss("cash-out-feature-button");
   });
 
 // Toggle and Show Transfer feature ///
@@ -109,6 +173,7 @@ document
   .getElementById("trasfer-money-freature-page")
   .addEventListener("click", function () {
     toggleButton("transfer-money-feature-section");
+    toggleForCss("trasfer-money-freature-page");
   });
 
 // Get Bonus site Toggle ///
@@ -116,7 +181,8 @@ document
 document
   .getElementById("get-bonus-feature-site")
   .addEventListener("click", function () {
-   toggleButton("get-bonus-feature-section");
+    toggleButton("get-bonus-feature-section");
+    toggleForCss("get-bonus-feature-site");
   });
 
 // Pay Bill site Toggle ///
@@ -124,7 +190,8 @@ document
 document
   .getElementById("pay-bill-feature-site")
   .addEventListener("click", function () {
-  toggleButton("pay-bill-feature-section");
+    toggleButton("pay-bill-feature-section");
+    toggleForCss("pay-bill-feature-site");
   });
 // Transaction History site Toggle ///
 
@@ -132,4 +199,5 @@ document
   .getElementById("transaction-history-feature-site")
   .addEventListener("click", function () {
     toggleButton("transaction-history-feature-section");
+    toggleForCss("transaction-history-feature-site");
   });
